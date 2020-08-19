786d54637c57508051d6254ea211e9e371426674
KAMAL <mkamals1989@gmail.com>
2020-08-20 00:22:18 +0530

    Initial Commit

---
+++ app/src/main/java/com/mks/currencyexchange/ExchangerActivity.kt
@@ -0,0 +1,11 @@
+package com.mks.currencyexchange
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExchangerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exchanger)
+    }
+}
---
+++ app/src/main/java/com/mks/currencyexchange/MainActivity.kt
@@ -0,0 +1,2 @@
+package com.mks.currencyexchange
+
